e03f5c28cc8725d0dc66d7ac16e178d0ace7f25a
Javiermav1970 <115325333+Javiermav1970@users.noreply.github.com>
2026-09-13 23:55:41 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -12,11 +12,6 @@ app.use(cors()); // Permitir conexiones desde cualquier URL (importante para hos
 
 app.use(express.static(path.join(__dirname, 'public')));  
 
-// Parche definitivo de transmisión para el Favicon Cyberpunk en internet
-app.get('/favicon.ico', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'favicon.png'));
-});
-
 const server = http.createServer(app);
 // Configurar los WebSockets para aceptar tráficos remotos
 const io = new Server(server, {
